95a8f2d64dee20211bdb98d3c6e11201a3c9d57a
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-18 22:08:52 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,17 @@
+// Must not begin with a digit.
+// Name is case sensitive.
+// Should not be a keyword (like void).
+// White space are not allowed.
+// Can cointain alphabets ,$ ,_ and digits if other codition (1 to 4) are met.
+
+class variable
+{
+    public static void main(String[] args) 
+    {
+        int a=12;
+        int b=4;
+        int c=24;
+        int sum=a+b+c;
+        System.out.println("The sum of the Three variables are: "+sum);
+    }
+}
