1756c08d133ec1d650dac659a8a422eda59f9ddb
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-18 22:19:46 +0530

    Add files via upload

---
+++ Basics/Literale.java
@@ -0,0 +1,32 @@
+public class Literale {
+    public static void main(String[] args) {
+        byte age = 19;
+        System.out.println(age);
+
+        int age2 = 18;
+        System.out.println(age2);
+
+        short age3 = 89;
+        System.out.println(age3);
+
+        long ageLong = 300000000000000L;
+        System.out.println(ageLong);
+
+        char ch = 'N'; 
+        System.out.println(ch);
+
+        float f1 = 12.4f;
+        System.out.println(f1);
+
+        double d1 = 20.24D;
+        System.out.println(d1);
+
+        boolean m = true;
+        System.out.println(m);
+
+        String str = "Niyati";
+        System.out.println(str);
+
+
+    }
+}
