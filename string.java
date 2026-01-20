f7eb00ca40b3691c809b94d4b7a51605c938c5c0
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-20 23:22:36 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+
+        // Option 1
+        String name = new String("Niyati");
+        System.out.println(name);
+
+        // Option 2
+        String name1 = "Niyati";
+        System.out.println(name1);
+
+        // Option 3
+        Scanner sc = new Scanner(System.in);
+        String st = sc.next();
+        System.out.println(st);
+        String s = sc.nextLine();
+        System.out.println(s);
+
+        sc.close();
+    }
+}
