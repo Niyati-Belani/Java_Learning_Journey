65b62c67a02c5ded9b0f1f7082f3ed17152c2066
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-20 23:18:45 +0530

    Add files via upload

---
+++ Basics/Taking_InputInJava.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Taking_InputInJava {
+    public static void main(String[] args) {
+        System.out.println("Taking input from user...");
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter number 1:");
+        int a = sc.nextInt();
+
+        System.out.println("Enter number 2:");
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.println("The sum of two number is: "+ sum);
+
+        sc.close();
+    }
+}
