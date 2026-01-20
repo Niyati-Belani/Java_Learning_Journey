8f8098293d4a0c0822dae1b8cb217ebfe1f791ef
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-20 23:19:58 +0530

    Add files via upload

---
+++ Basics/Data_Type_Incre_Decre.java
@@ -0,0 +1,36 @@
+public class Data_Type_Incre_Decre {
+    public static void main(String[] args) {
+
+        // Data type Expressions
+
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        int i = 10;
+
+        //  Increment  
+        // Post
+        System.out.println(i++);
+        System.out.println(i);
+        //Pre
+        System.out.println(++i);
+        System.out.println(i);
+
+        int j = 12;
+
+        //Decrement
+        //post
+        System.out.println(j--);
+        System.out.println(j);
+        //Pre
+        System.out.println(--j);
+        System.out.println(j);
+        
+    }
+}
