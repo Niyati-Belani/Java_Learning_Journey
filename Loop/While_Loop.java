7be0c743a5e768690bdc3a6eb29d5f2d7555c448
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-22 23:12:31 +0530

    Create While_Loop.java

---
+++ Loop/While_Loop.java
@@ -0,0 +1,10 @@
+public class While_Loop {
+    public static void main(String[] args) {
+        System.out.println("I'm Using While Loop.");
+        int i=1;
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
