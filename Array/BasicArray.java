f575d1671d5ac7adb5c0263e8c1807bb76e5b226
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-22 23:04:57 +0530

    Update BasicArray.java

--- Array/BasicArray.java
+++ Array/BasicArray.java
@@ -1,18 +1,28 @@
-package Array;
+public class BasicArray 
+{
+    public static void main(String[] args) 
+    {
+            //Method1
+            int [] marks = new int [5];
+            marks[0] = 100;
+            marks[1] = 99;
+            marks[2] = 95;
+            marks[3] = 85;
+            marks[4] = 80;
+            System.out.println(marks[4]);
 
-public class BasicArray {
-    static public void main(String[] args) {
-        
-        int[] marks = new int[5] ;
+            //Method2
+            int [] student ;
+            student = new int [5];
+            student[0] = 1;
+            student[1] = 2;
+            student[2] = 3;
+            student[3] = 4;
+            student[4] = 5;
+            System.out.println(student[2]);
 
-        marks[0]=78;
-        marks[1]=80;
-        marks[2]=90;
-        marks[3]=96;
-        marks[4]=69;
-
-        for (int i = 0; i < marks.length; i++) {
-            System.out.println( i + ": " + marks[i]);
-        }
+            //Method3
+            int [] city = {1 , 2 , 3};
+            System.out.println(city[1]);            
     }
 }
