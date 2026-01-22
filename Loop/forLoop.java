a8b96f6b46c82fab7211814488648091144e3ba8
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-22 23:14:54 +0530

    Add files via upload

---
+++ Loop/forLoop.java
@@ -0,0 +1,17 @@
+public class forLoop 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("First 10 Odd Numbers: ");
+        for(int i = 0;i <= 10;i++)
+        {
+            System.out.println(2*i+1);
+        }
+
+        System.out.println("Reverse Numbers form 5.");
+        for(int j = 5;j >=0;j--)
+        {
+            System.out.println(j);
+        }
+    }
+}
