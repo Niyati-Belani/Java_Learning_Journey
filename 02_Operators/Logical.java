7ee96309145569d319ea7b5327a6f5e8b7f728a1
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-26 23:11:12 +0530

    Add files via upload

---
+++ 02_Operators/Logical.java
@@ -0,0 +1,38 @@
+public class Logical 
+{
+    public static void main(String[] args) 
+    {
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        // For Logical AND(&&)...
+        System.out.println("For Logical AND(&&)...");
+        if(a && b && c)
+        {
+            System.out.println("Yes!");
+        }
+        else
+        {
+            System.out.println("No!");
+        }
+
+        // For Logical OR(||)...
+        System.out.println("For Logical OR(||)...");
+         if(a || b)
+        {
+            System.out.println("Yes!");
+        }
+        else
+        {
+            System.out.println("No!");
+        }
+
+        // For Logical NOT(!)...
+        System.out.println("For Logical NOT(!)...");
+        System.out.print("Not (a) is : ");
+        System.out.println(!a);
+        System.out.print("Not (b) is : ");
+        System.out.println(!b);
+    }
+}
