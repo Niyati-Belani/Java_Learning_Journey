40be8d2c87288116a9ddf6a7c4364617fa983059
Niyati Belani <niyatibelani1020@gmail.com>
2026-01-26 23:11:53 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,15 @@
+public class Conditional 
+{
+    public static void main(String[] args) 
+    {
+        int age = 16;
+        if(age>18)
+        {
+            System.out.println("Yes you can Drive!");
+        }
+        else
+        {
+            System.out.println("No you cann't Drive!");
+        }
+    }
+}
